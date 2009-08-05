9e578e49541198a807e2b4f8f6e43576426dd3c8
Stefan H. Holek <stefan@epy.co.at>
2009-08-05 13:48:00 +0200

    Implement unescape.

--- gpgkeys/escape.py
+++ gpgkeys/escape.py
@@ -1,31 +1,72 @@
 import os
 
 
+def startidx(s, idx=-1):
+    n = len(s)
+    if idx < 0:
+        idx = n + idx
+    i = min(idx, n-1)
+    while i >= 0:
+        if s[i] == os.sep:
+            return i+1
+        if s[i] == ' ' and s[max(i-1, 0)] != '\\':
+            return i+1
+        i -= 1
+    return 0
+
+
+def get_quote_char(s):
+    lx = len(s)-1
+
+    dq = s.find('"')
+    while dq > 0 and s[dq-1] == '\\' and dq < lx:
+        dq = s.find('"', dq+1)
+
+    sq = s.find("'")
+    while sq > 0 and s[sq-1] == '\\' and sq < lx:
+        sq = s.find("'", sq+1)
+
+    if dq >= 0 and (sq < 0 or sq > dq):
+        return '"'
+    if sq >= 0 and (dq < 0 or dq > sq):
+        return "'"
+    return '\\'
+
+
 def escape(s):
     if '\\ ' in s:
         return s
     return s.replace(' ', '\\ ')
 
 
-def unescape(s):
-    return s.replace('\\ ', ' ')
+def unescape(args):
+    qc = get_quote_char(args)
+    if qc in ('"', "'"):
+        return q_unescape(args, qc)
+    return bs_unescape(args)
+
+
+def bs_unescape(args):
+    if len(args) > 1:
+        args = args.replace('\\ ', ' ')
+        args = args.replace('\\"', '"')
+        args = args.replace("'\\''", "'")
+    return args
+
+
+def q_unescape(args, quote_char):
+    if len(args) > 1:
+        if args[-1] == quote_char and args[-2] != '\\':
+            args = args[:-1]
+        if args[0] == quote_char:
+            args = args[1:]
+    return bs_unescape(args)
 
 
 def split(args):
-    lx = len(args)-1
-
-    dq = args.find('"')
-    while dq > 0 and args[dq-1] == '\\' and dq < lx:
-        dq = args.find('"', dq+1)
-
-    sq = args.find("'")
-    while sq > 0 and args[sq-1] == '\\' and sq < lx:
-        sq = args.find("'", sq+1)
-
-    if dq >= 0 and (sq < 0 or sq > dq):
-        return q_split(args, '"')
-    if sq >= 0 and (dq < 0 or dq > sq):
-        return q_split(args, "'")
+    qc = get_quote_char(args)
+    if qc in ('"', "'"):
+        return q_split(args, qc)
     return bs_split(args)
 
 
@@ -61,18 +102,4 @@ def q_split(args, quote_char):
     if n:
         r.append(args[j:])
     return tuple(r)
-
-
-def startidx(s, idx=-1):
-    n = len(s)
-    if idx < 0:
-        idx = n + idx
-    i = min(idx, n-1)
-    while i >= 0:
-        if s[i] == os.sep:
-            return i+1
-        if s[i] == ' ' and s[max(i-1, 0)] != '\\':
-            return i+1
-        i -= 1
-    return 0
 
