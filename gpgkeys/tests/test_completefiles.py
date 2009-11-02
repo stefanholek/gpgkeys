8f94c52fd99777ed6056df885082fa1d0b46084b
Stefan H. Holek <stefan@epy.co.at>
2009-11-02 18:48:50 +0100

    Add some more filename completion tests.

--- gpgkeys/tests/test_completefiles.py
+++ gpgkeys/tests/test_completefiles.py
@@ -1,59 +1,125 @@
+# -*- coding: utf-8 -*-
+
 import os
 import unittest
-
-from os.path import dirname
-
-from gpgkeys.gpgkeys import GPGKeys
-from gpgkeys.completions.filename import FilenameCompletion
-from gpgkeys.testing import TreeSetup
 
 from rl import completer
 from rl import completion
 from rl import readline
 
+from gpgkeys.gpgkeys import GPGKeys
+from gpgkeys.completions.filename import FilenameCompletion
+from rl.testing import JailSetup
 
-class CompleterTests(TreeSetup):
+TAB = '\t'
+
+
+class CompleterTests(JailSetup):
 
     def setUp(self):
-        TreeSetup.setUp(self)
+        JailSetup.setUp(self)
+        self.mkfiles()
         self.cmd = GPGKeys()
         self.cmd.init_completer()
         self.completefilenames = FilenameCompletion(quote_char='"')
         completer.completer = self.cmd.complete
-        os.chdir('normaldir')
+
+    def mkfiles(self):
+        self.mkfile("Al'Hambra.txt")
+        self.mkfile('Foo\\"Peng\\".txt')
+        self.mkfile('Foo\\Bar.txt')
+        self.mkfile('Foo\\Baz.txt')
+        self.mkfile('Hello World.txt')
+        self.mkfile('Lee "Scratch" Perry.txt')
+        self.mkfile('Mädchen.txt')
+        self.mkfile('Simple.txt')
+        self.mkfile('Tilde.tx~')
+        self.mkfile('~StartsWithTilde.txt')
 
     def complete(self, text):
         completion.line_buffer = text
-        readline.complete_internal('\t')
+        readline.complete_internal(TAB)
         return completion.line_buffer
 
     def test_simple(self):
         self.assertEqual(self.complete('fdump Simple'),
-                         'fdump Simple.txt ')
+                                       'fdump Simple.txt ')
 
     def test_hello(self):
         self.assertEqual(self.complete('fdump Hell'),
-                         'fdump "Hello World.txt" ')
+                                       'fdump "Hello World.txt" ')
 
-    def test_hello_quote(self):
+    def test_hello_double_quote(self):
         self.assertEqual(self.complete('fdump "Hello '),
-                         'fdump "Hello World.txt" ')
+                                       'fdump "Hello World.txt" ')
 
     def test_hello_single_quote(self):
         self.assertEqual(self.complete("fdump 'Hello "),
-                         "fdump 'Hello World.txt' ")
+                                       "fdump 'Hello World.txt' ")
 
     def test_lee(self):
         self.assertEqual(self.complete('fdump Lee'),
-                         'fdump "Lee \\"Scratch\\" Perry.txt" ')
+                                       'fdump "Lee \\"Scratch\\" Perry.txt" ')
 
-    def test_lee_quote(self):
-        self.assertEqual(self.complete('''fdump "Lee \\"'''),
-                         'fdump "Lee \\"Scratch\\" Perry.txt" ')
+    def test_lee_double_quote(self):
+        self.assertEqual(self.complete('fdump "Lee \\"'),
+                                       'fdump "Lee \\"Scratch\\" Perry.txt" ')
 
     def test_lee_single_quote(self):
         self.assertEqual(self.complete('''fdump 'Lee "'''),
-                         '''fdump 'Lee "Scratch" Perry.txt' ''')
+                                       '''fdump 'Lee "Scratch" Perry.txt' ''')
+
+    def test_foobar(self):
+        self.assertEqual(self.complete('fdump Foo'),
+                                       'fdump Foo\\\\')
+
+    def test_foobar_double_quote(self):
+        self.assertEqual(self.complete('fdump "Foo'),
+                                       'fdump "Foo\\\\')
+
+    def test_foobar_single_quote(self):
+        self.assertEqual(self.complete("fdump 'Foo"),
+                                       "fdump 'Foo\\")
+
+    def test_alhambra(self):
+        self.assertEqual(self.complete('fdump Al'),
+                                     '''fdump "Al'Hambra.txt" ''')
+
+    def test_alhambra_double_quote(self):
+        self.assertEqual(self.complete('fdump "Al'),
+                                     '''fdump "Al'Hambra.txt" ''')
+
+    def test_alhambra_single_quote(self):
+        self.assertEqual(self.complete("fdump 'Al"),
+                                       "fdump 'Al'\\''Hambra.txt' ")
+
+    def test_foopeng(self):
+        self.assertEqual(self.complete('fdump Foo\\\\\\"'),
+                                       'fdump Foo\\\\\\"Peng\\\\\\".txt ')
+
+    def test_foopeng_double_quote(self):
+        self.assertEqual(self.complete('fdump "Foo\\\\\\"'),
+                                       'fdump "Foo\\\\\\"Peng\\\\\\".txt" ')
+
+    def test_foopeng_single_quote(self):
+        self.assertEqual(self.complete('''fdump 'Foo\\"'''),
+                                       '''fdump 'Foo\\"Peng\\".txt' ''')
+
+    def test_tilde(self):
+        self.assertEqual(self.complete('fdump Tilde'),
+                                       'fdump Tilde.tx~ ')
+
+    def test_starts_with_tilde(self):
+        self.assertEqual(self.complete('fdump ~Starts'),
+                                       'fdump ~StartsWithTilde.txt ')
+
+    def test_returned_umlaut(self):
+        self.assertEqual(self.complete('fdump M'),
+                                       'fdump Mädchen.txt ')
+
+    def test_typed_umlaut(self):
+        self.assertEqual(self.complete('fdump Mä'),
+                                       'fdump Mädchen.txt ')
 
 
 class CharIsQuotedTests(unittest.TestCase):
