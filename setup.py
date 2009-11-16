3760124afac7cf25c03dd0a1b6c9c3796afffc1e
Stefan H. Holek <stefan@epy.co.at>
2009-11-16 13:10:25 +0100

    Prepare gpgkeys 1.1.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.0.1'
+version = '1.1'
 
 
 setup(name='gpgkeys',
@@ -26,7 +26,7 @@ setup(name='gpgkeys',
       test_suite='gpgkeys.tests',
       install_requires=[
           'setuptools',
-          'rl',
+          'rl > 1.0',
       ],
       entry_points = {
           'console_scripts': 'gpgkeys=gpgkeys.gpgkeys:main',
