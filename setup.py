2643efb7045797fade0b7b4855f38fe0e6c83d40
Stefan H. Holek <stefan@epy.co.at>
2010-03-07 12:08:32 +0100

    Prepare gpgkeys 1.9.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='gpgkeys',
       test_suite='gpgkeys.tests',
       install_requires=[
           'setuptools',
-          'rl >= 1.6dev',
+          'rl >= 1.6',
       ],
       entry_points = {
           'console_scripts': 'gpgkeys=gpgkeys.gpgkeys:main',
