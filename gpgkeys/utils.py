8d61ecfaedcbdb9dc61f6ffbae6224c3ef879a87
Stefan H. Holek <stefan@epy.co.at>
2010-04-22 19:40:30 +0200

    Add b() function to fake byte literals.

--- gpgkeys/utils.py
+++ gpgkeys/utils.py
@@ -1,9 +1,11 @@
 import sys
 
+PY3 = sys.version_info[0] >= 3
+
 
 def decode(text):
     """Decode from the charset of the current locale."""
-    if sys.version_info[0] >= 3:
+    if PY3:
         return text.decode(sys.getfilesystemencoding(), 'surrogateescape')
     else:
         return text.decode(sys.getfilesystemencoding(), 'replace')
@@ -11,8 +13,16 @@ def decode(text):
 
 def encode(text):
     """Encode to the charset of the current locale."""
-    if sys.version_info[0] >= 3:
+    if PY3:
         return text.encode(sys.getfilesystemencoding(), 'surrogateescape')
     else:
         return text.encode(sys.getfilesystemencoding(), 'replace')
+
+
+def b(text):
+    """Python 2.5 doesn't know about byte literals."""
+    if PY3:
+        return text.encode('utf-8')
+    else:
+        return text
 
