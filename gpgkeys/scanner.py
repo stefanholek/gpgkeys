0fbf56078bdfa6864f5d0c67def67725e9b3322f
Stefan H. Holek <stefan@epy.co.at>
2011-11-02 12:19:36 +0100

    Change order of functions in module.

--- gpgkeys/scanner.py
+++ gpgkeys/scanner.py
@@ -2,6 +2,29 @@ WHITESPACE = (' ', '\t', '\n')
 QUOTECHARS = ('"', "'")
 
 
+def char_is_quoted(s, x):
+    """Return True if the character at x is quoted.
+    """
+    skip_next = False
+    quote_char = ''
+    for i in range(x):
+        c = s[i]
+        if skip_next:
+            skip_next = False
+        elif quote_char != "'" and c == '\\':
+            skip_next = True
+            if i == x-1:
+                return True
+        elif quote_char != '':
+            if c == quote_char:
+                quote_char = ''
+        elif c in QUOTECHARS:
+            quote_char = c
+    if x < len(s) and s[x] == quote_char:
+        return False
+    return bool(quote_char)
+
+
 def scan_unquoted(s, lx, chars):
     """Find any one of the characters in chars before lx.
     """
@@ -46,27 +69,4 @@ def rscan_unquoted(s, lx, chars):
         elif c in chars:
             result = i
     return result
-
-
-def char_is_quoted(s, x):
-    """Return True if the character at x is quoted.
-    """
-    skip_next = False
-    quote_char = ''
-    for i in range(x):
-        c = s[i]
-        if skip_next:
-            skip_next = False
-        elif quote_char != "'" and c == '\\':
-            skip_next = True
-            if i == x-1:
-                return True
-        elif quote_char != '':
-            if c == quote_char:
-                quote_char = ''
-        elif c in QUOTECHARS:
-            quote_char = c
-    if x < len(s) and s[x] == quote_char:
-        return False
-    return bool(quote_char)
 
