90d431c0e750e8ea84c97e708167ff8025bec6bc
Stefan H. Holek <stefan@epy.co.at>
2011-11-02 16:14:49 +0100

    Use dev friendly version spec.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='gpgkeys',
       test_suite='gpgkeys.tests',
       install_requires=[
           'setuptools',
-          'kmd >= 2.0.1',
+          'kmd > 2.0',
       ],
       entry_points = {
           'console_scripts': 'gpgkeys=gpgkeys.gpgkeys:main',
