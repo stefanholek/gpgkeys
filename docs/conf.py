06c883f0623ab6ee36df096073d72cbcd096a059
Stefan H. Holek <stefan@epy.co.at>
2011-11-06 12:23:34 +0100

    Fix copyright period in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'gpgkeys'
-copyright = u'%s, Stefan H. Holek' % time.strftime('%Y')
+copyright = u'2009-%s, Stefan H. Holek' % time.strftime('%Y')
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
