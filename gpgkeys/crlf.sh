3665874d29ea266536d3557deb1243ba490df349
Stefan H. Holek <stefan@epy.co.at>
2012-04-06 19:48:37 +0200

    Sync with getyx.

--- gpgkeys/crlf.sh
+++ gpgkeys/crlf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-printf "\033[6n" &
+printf "\033[6n" > /dev/tty &
 read -sdR -t1 POS
-rc=$?
-if [ $rc -eq 0 ]; then
-    POS=${POS##*[}
-    ROW=${POS%%;*}
-    COL=${POS##*;}
-    if [ ${COL:-0} -gt 1 ]; then
+RC=$?
+if [ $RC -eq 0 ]; then
+    POS="${POS##*[}"
+    ROW="${POS%%;*}"
+    COL="${POS##*;}"
+    if [ $COL -gt 1 ]; then
         echo
     fi
 fi
-exit $rc
+exit $RC
