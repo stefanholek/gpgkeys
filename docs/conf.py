2bf66a49861fac40ac698fedb57e16ac0b639a2e
Stefan H. Holek <stefan@epy.co.at>
2012-05-18 22:47:11 +0200

    No environment vars in conf.py either.

--- docs/conf.py
+++ docs/conf.py
@@ -91,11 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-if os.environ.get('READTHEDOCS'):
-    html_theme = 'default'
-else:
-    html_theme = 'nature'
-    html_style = 'nature-plus.css'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
